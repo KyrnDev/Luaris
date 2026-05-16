7981f0ad1a9d609aa86cebc0a2568811c06fe84e
Danny <danny.smc95@gmail.com>
2026-05-16 01:48:48 +0100

    Fixing stupid icons.... take 3

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 	},
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
-		logo: '/Luaris/logo.png',
+		logo: '/logo.png',
 
 		nav: [
 			{ text: 'Home', link: '/' },
